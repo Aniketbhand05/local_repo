3b2ccc546dd534b2bdc9256e77e73c313fe5c529
Aniketbhand05 <aniketbhand05@gmail.com>
2023-08-31 20:21:44 +0530

    added two more print statements

--- first.java
+++ first.java
@@ -3,6 +3,8 @@ package localrepo;
 public class first {
     public static void main(String args[]){
         System.out.println("hello world");
+        System.out.print("hello moon ");
+        System.out.println("its chandrayaan here");
     }
     
 }
